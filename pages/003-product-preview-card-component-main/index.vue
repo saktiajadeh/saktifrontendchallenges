e7f707f4a6cb2475c6c4b2e46dccbbec08e161fe
Sakti <saktiajadeh29@gmail.com>
2024-01-06 17:30:58 +0800

    finishing challenges 003-product-preview-card-component-main

--- pages/003-product-preview-card-component-main/index.vue
+++ pages/003-product-preview-card-component-main/index.vue
@@ -1,15 +1,38 @@
 <template>
-    Perfume
-
-    Gabrielle Essence Eau De Parfum
-
-    A floral, solar and voluptuous interpretation composed by Olivier Polge,
-    Perfumer-Creator for the House of CHANEL.
-
-    $149.99
-    $169.99
-
-    Add to Cart
+    <div class="body">
+        <div class="container">
+            <div class="wrap-card">
+                <div class="wrap-img">
+                    <img src=".\images\image-product-desktop.jpg" alt="Image Product" class="img-desktop">
+                    <img src=".\images\image-product-mobile.jpg" alt="Image Product" class="img-mobile">
+                </div>
+                <div class="wrap-content">
+                    <div class="wrap-tag">
+                        <a href="https://www.google.com/" target="_blank" class="tag-item">
+                            Perfume
+                        </a>
+                    </div>
+                    <div class="wrap-title">
+                        Gabrielle Essence Eau De Parfum
+                    </div>
+                    <div class="wrap-description">
+                        A floral, solar and voluptuous interpretation composed by Olivier Polge,
+                        Perfumer-Creator for the House of CHANEL.
+                    </div>
+                    <div class="wrap-price">
+                        <span class="show-price">$149.99</span>
+                        <span class="disc-price">$169.99</span>
+                    </div>
+                    <div class="wrap-action">
+                        <a href="https://www.google.com/" target="_blank" class="wrap-button">
+                            <img src=".\images\icon-cart.svg" alt="Icon Cart">
+                            <span>Add to Cart</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
@@ -18,6 +41,6 @@
 
 <script setup>
     useHead({
-        title: '001 Blog Preview Card',
+        title: '003 Product preview card component',
     })
 </script>
