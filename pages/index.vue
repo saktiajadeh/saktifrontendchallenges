ef926474a526c72158b822ca93adc966b8e32132
Sakti <saktiajadeh29@gmail.com>
2024-01-06 19:26:44 +0800

    renaming -main pages

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,9 @@
     <div class="body">
         <div class="container">
             <ul class="styled-list">
-                <li><NuxtLink to="/001-blog-preview-card-main">001 Blog Preview Card</NuxtLink></li>
-                <li><NuxtLink to="/002-results-summary-component-main">002 Results Summary Component</NuxtLink></li>
-                <li><NuxtLink to="/003-product-preview-card-component-main">003 Product preview card component</NuxtLink></li>
+                <li><NuxtLink to="/001-blog-preview-card">001 Blog Preview Card</NuxtLink></li>
+                <li><NuxtLink to="/002-results-summary-component">002 Results Summary Component</NuxtLink></li>
+                <li><NuxtLink to="/003-product-preview-card-component">003 Product preview card component</NuxtLink></li>
             </ul>
             <div class="attribution">
                 Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
