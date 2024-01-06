e1b725e824c769a59707556598031697f4b0aecc
Sakti <saktiajadeh29@gmail.com>
2024-01-06 15:38:20 +0800

    add started files 003-product-preview-card-component-main

---
+++ pages/003-product-preview-card-component-main/index.vue
@@ -0,0 +1,23 @@
+<template>
+    Perfume
+
+    Gabrielle Essence Eau De Parfum
+
+    A floral, solar and voluptuous interpretation composed by Olivier Polge,
+    Perfumer-Creator for the House of CHANEL.
+
+    $149.99
+    $169.99
+
+    Add to Cart
+</template>
+
+<style lang="scss" scoped>
+    @import'./style.scss';
+</style>
+
+<script setup>
+    useHead({
+        title: '001 Blog Preview Card',
+    })
+</script>
