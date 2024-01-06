0985fb3def098ecd7538d720e929884a32f37516
saktiajadeh <saktiajadeh29@gmail.com>
2024-01-06 22:56:27 +0800

    finishing challenges 005-nft-preview-card-component

--- pages/005-nft-preview-card-component/index.vue
+++ pages/005-nft-preview-card-component/index.vue
@@ -1,43 +1,48 @@
-
 <template>
-  <div class="body">
-      <div class="container">
-          <div class="wrap-card">
-              <div class="wrap-img">
-                  <img src=".\images\image-product-desktop.jpg" alt="Image Product" class="img-desktop">
-                  <img src=".\images\image-product-mobile.jpg" alt="Image Product">
-              </div>
-              <div class="wrap-content">
-                  <div class="wrap-title">
-                  </div>
-                  <div class="wrap-description">
-                    Equilibrium #3429
-
-                    Our Equilibrium collection promotes balance and calm.
-
-                    0.041 ETH
-                    3 days left
-
-                    Creation of Jules Wyvern
-                  </div>
-                  <div class="wrap-action">
-                      <a href="https://www.google.com/" target="_blank" class="wrap-button">
-                          <img src=".\images\icon-cart.svg" alt="Icon Cart">
-                          <span>Add to Cart</span>
-                      </a>
-                  </div>
-              </div>
-          </div>
-      </div>
-  </div>
+    <div class="body">
+        <div class="container">
+            <div class="wrap-card">
+                <a href="https://www.google.com/" target="_blank" class="wrap-img">
+                    <img src=".\images\image-equilibrium.jpg" alt="Image Product">
+                    <div class="active-states">
+                        <img src=".\images\icon-view.svg" alt="Icon View">
+                    </div>
+                </a>
+                <div class="wrap-content">
+                    <a href="https://www.google.com/" target="_blank" class="wrap-title">
+                        Equilibrium #3429
+                    </a>
+                    <div class="wrap-description">
+                        Our Equilibrium collection promotes balance and calm.
+                    </div>
+                    <div class="wrap-price-with-icon">
+                        <span class="show-price">
+                            <img src=".\images\icon-ethereum.svg" alt="">
+                            0.041 ETH
+                        </span>
+                        <span class="show-info">
+                            <img src=".\images\icon-clock.svg" alt="">
+                            3 days left
+                        </span>
+                    </div>
+                    <div class="separator"></div>
+                    <a href="https://www.google.com/" target="_blank" class="wrap-info-with-thumb">
+                        <img src=".\images\image-avatar.png" alt="">
+                        <span>Creation of</span>&nbsp;
+                        <span class="name">Jules Wyvern</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
-  @import'./style.scss';
+    @import'./style.scss';
 </style>
 
 <script setup>
-  useHead({
-      title: '005 NFT preview card component',
-  })
+    useHead({
+        title: '005 NFT preview card component',
+    })
 </script>
