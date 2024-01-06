38fac0263233a11d2a6cb259d49327655a2aff85
saktiajadeh <saktiajadeh29@gmail.com>
2024-01-06 21:43:18 +0800

    add started files 004-qr-code-component

---
+++ pages/004-qr-code-component/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="body">
+      <div class="container">
+          <div class="wrap-card">
+              <div class="wrap-img">
+                  <img src=".\images\image-product-desktop.jpg" alt="Image QR Code">
+              </div>
+              <div class="wrap-content">
+                  <div class="wrap-title">
+                      Improve your front-end skills by building projects
+                  </div>
+                  <div class="wrap-description">
+                      Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  @import'./style.scss';
+</style>
+
+<script setup>
+  useHead({
+      title: '004 QR code component',
+  })
+</script>
