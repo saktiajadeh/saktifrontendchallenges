f89d44de7fda274ed992dae2754bcbd925dfabff
Sakti <saktiajadeh29@gmail.com>
2024-01-06 19:24:18 +0800

    add transition app

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   // devtools: { enabled: true },
-  css: ['~/assets/scss/main.scss']
+  css: ['~/assets/scss/main.scss'],
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' }
+  },
 })
