40ab192bdc5820700eb139954f7c582c1c91bc80
Sakti <saktiajadeh29@gmail.com>
2024-01-07 19:08:19 +0800

    finishing challenges 008-3-column-preview-card-component

--- pages/008-3-column-preview-card-component/index.vue
+++ pages/008-3-column-preview-card-component/index.vue
@@ -1,56 +1,80 @@
 <template>
-  <div class="body">
-      <div class="container">
-          <div class="wrap-card">
-              <div class="wrap-img">
-                  <img src=".\images\image-header-desktop.jpg" alt="Image Preview">
-                    <div class="active-states">
+    <div class="body">
+        <div class="container">
+            <div class="container-card">
+                <div class="wrap-card brightorange">
+                    <div class="wrap-content">
+                        <div class="spacer">
+                            <div class="wrap-img">
+                                <img src=".\images\icon-sedans.svg" alt="Icon Preview">
+                            </div>
+                            <div class="wrap-title">
+                                Sedans
+                            </div>
+                            <div class="wrap-description">
+                                Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city
+                                or on your next road trip.
+                            </div>
+                        </div>
+                        <div class="wrap-action">
+                            <a href="https://www.google.com/" target="_blank" class="wrap-button primary">
+                                <span>Learn More</span>
+                            </a>
+                        </div>
                     </div>
-              </div>
-              <div class="wrap-content">
-                  <div class="wrap-title">
-                    Sedans
-                    Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city 
-                    or on your next road trip.
-
-                    SUVs
-                    Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation 
-                    and off-road adventures.
-
-                    Luxury
-                    Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury 
-                    rental and arrive in style.
-                  </div>
-                  <div class="wrap-description">
-                    Discover the benefits of data analytics and make better decisions regarding revenue, customer 
-                    experience, and overall efficiency.
-                  </div>
-                  <div class="wrap-stats">
-                    <div class="stats-item">
-                      <div class="title">10k+</div>
-                      <div class="desc">companies</div>
+                </div>
+                <div class="wrap-card darkcyan">
+                    <div class="wrap-content">
+                        <div class="spacer">
+                            <div class="wrap-img">
+                                <img src=".\images\icon-suvs.svg" alt="Icon Preview">
+                            </div>
+                            <div class="wrap-title">
+                                SUVs
+                            </div>
+                            <div class="wrap-description">
+                                Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.
+                            </div>
+                        </div>
+                        <div class="wrap-action">
+                            <a href="https://www.google.com/" target="_blank" class="wrap-button primary">
+                                <span>Learn More</span>
+                            </a>
+                        </div>
                     </div>
-                    <div class="stats-item">
-                      <div class="title">314</div>
-                      <div class="desc">templates</div>
+                </div>
+                <div class="wrap-card verydarkcyan">
+                    <div class="wrap-content">
+                        <div class="spacer">
+                            <div class="wrap-img">
+                                <img src=".\images\icon-luxury.svg" alt="Icon Preview">
+                            </div>
+                            <div class="wrap-title">
+                                Luxury
+                            </div>
+                            <div class="wrap-description">
+                                Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury
+                                rental and arrive in style.
+                            </div>
+                        </div>
+                        <div class="wrap-action">
+                            <a href="https://www.google.com/" target="_blank" class="wrap-button primary">
+                                <span>Learn More</span>
+                            </a>
+                        </div>
                     </div>
-                    <div class="stats-item">
-                      <div class="title">12m+</div>
-                      <div class="desc">queries</div>
-                    </div>
-                  </div>
-              </div>
-          </div>
-      </div>
-  </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
-  @import'./style.scss';
+    @import'./style.scss';
 </style>
 
 <script setup>
-  useHead({
-      title: '007 Stats preview card component',
-  })
+    useHead({
+        title: '008 3-column preview card component',
+    })
 </script>
