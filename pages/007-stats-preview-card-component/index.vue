51fc29e34e29c58e24994f5043226ef7122110a9
saktiajadeh <saktiajadeh29@gmail.com>
2024-01-07 16:40:59 +0800

    add started files 007-stats-preview-card-component

---
+++ pages/007-stats-preview-card-component/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="body">
+      <div class="bg-pattern-1">
+          <img src=".\images\pattern-background-desktop.svg" alt="BG Pattern 1">
+      </div>
+      <div class="bg-pattern-2">
+          
+      </div>
+      <div class="container">
+          <div class="wrap-card">
+              <div class="wrap-img">
+                  <img src=".\images\illustration-hero.svg" alt="Image Product">
+              </div>
+              <div class="wrap-content">
+                  <div class="wrap-title">
+                      Order Summary
+                  </div>
+                  <div class="wrap-description">
+                    Get insights that help your business grow.
+
+                    Discover the benefits of data analytics and make better decisions regarding revenue, customer 
+                    experience, and overall efficiency.
+
+                    10k+ companies
+                    314 templates
+                    12m+ queries
+                  </div>
+                  <div class="wrap-price-with-icon">
+                      <div class="show-price">
+                          <img src=".\images\icon-music.svg" alt="">
+                          <div>
+                              <span>Annual Plan</span>
+                              <span>$59.99/year</span>
+                          </div>
+                      </div>
+                      <a href="https://www.google.com/" target="_blank" class="show-info">
+                          Change
+                      </a>
+                  </div>
+                  <div class="wrap-action">
+                      <a href="https://www.google.com/" target="_blank" class="wrap-button primary">
+                          <span>Proceed to Payment</span>
+                      </a>
+                      <a href="https://www.google.com/" target="_blank" class="wrap-button cancel">
+                          <span>Cancel Order</span>
+                      </a>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  @import'./style.scss';
+</style>
+
+<script setup>
+  useHead({
+      title: '007 Stats preview card component',
+  })
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,7 @@
                 <li><NuxtLink to="/004-qr-code-component">004 QR code component</NuxtLink></li>
                 <li><NuxtLink to="/005-nft-preview-card-component">005 NFT preview card component</NuxtLink></li>
                 <li><NuxtLink to="/006-order-summary-component">006 Order summary component</NuxtLink></li>
+                <li><NuxtLink to="/007-stats-preview-card-component">007 Stats preview card component</NuxtLink></li>
             </ul>
             <div class="attribution">
                 Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
