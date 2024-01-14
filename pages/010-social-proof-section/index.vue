8dc1d51122d5a2aad1dd17397bee962eba5ca245
saktiajadeh <saktiajadeh29@gmail.com>
2024-01-14 15:50:01 +0800

    finishing challenges 012-single-price-grid-component

--- pages/010-social-proof-section/index.vue
+++ pages/010-social-proof-section/index.vue
@@ -1,113 +1,143 @@
 <template>
-  <div class="body">
-      <div class="container">
-          <div class="container-card">
-              <div class="wrap-card bg-white">
-                  <div class="wrap-content">
-                      <div class="spacer">
-                          <div class="wrap-title f-cyan fw-700">
-                              Join our community
-                          </div>
-                          <div class="wrap-description mt-20">
-                              <span class="f-brightyellow fw-700">
-                                  30-day, hassle-free money back guarantee
-                              </span>
-                              <span class="f-grayishblue fw-400 d-block mt-20">
-                                10,000+ of our users love our products.
-
-                                We only provide great products combined with excellent customer service.
-                                See what our satisfied customers are saying about our services.
-
-                                Rated 5 Stars in Reviews
-                                Rated 5 Stars in Report Guru
-                                Rated 5 Stars in BestTech
-
-                                Colton Smith 
-                                Verified Buyer
-                                "We needed the same printed design as the one we had ordered a week prior.
-                                Not only did they find the original order, but we also received it in time.
-                                Excellent!"
-
-                                Irene Roberts 
-                                Verified Buyer
-                                "Customer service is always excellent and very quick turn around. Completely
-                                delighted with the simplicity of the purchase and the speed of delivery."
-
-                                Anne Wallace 
-                                Verified Buyer
-                                "Put an order with this company and can only praise them for the very high
-                                standard. Will definitely use them again and recommend them to everyone!"
-                              </span>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-              <div class="wrap-card bg-cyan">
-                  <div class="wrap-content">
-                      <div class="spacer">
-                          <div class="wrap-title f-white fw-700">
-                              Monthly Subscription
-                          </div>
-                          <div class="wrap-price f-white mt-20">
-                              <span class="show-price fw-700">$29</span>
-                              <span class="disc-price ml-10">per month</span>
-                          </div>
-                          <div class="wrap-description mt-10">
-                              <span class="f-white fw-400 d-block mt-10">
-                                  Full access for less than $1 a day
-                              </span>
-                          </div>
-                      </div>
-                      <div class="wrap-action mt-20">
-                          <a href="https://www.google.com/" target="_blank" class="wrap-button bg-brightyellow f-white fw-700">
-                              <span>Sign Up</span>
-                          </a>
-                      </div>
-                  </div>
-              </div>
-              <div class="wrap-card bg-cyan70">
-                  <div class="wrap-content">
-                      <div class="spacer">
-                          <div class="wrap-title f-white fw-700">
-                              Why Us
-                          </div>
-                          <div class="wrap-description mt-20">
-                              <a href="https://www.google.com/" target="_blank" class="f-white75 fw-400 d-block t-decor-none">
-                                  Tutorials by industry experts
-                              </a>
-                              <a href="https://www.google.com/" target="_blank" class="f-white75 fw-400 d-block t-decor-none mt-5">
-                                  Peer & expert code reviews
-                              </a>
-                              <a href="https://www.google.com/" target="_blank" class="f-white75 fw-400 d-block t-decor-none mt-5">
-                                  Coding exercises
-                              </a>
-                              <a href="https://www.google.com/" target="_blank" class="f-white75 fw-400 d-block t-decor-none mt-5">
-                                  Access to our GitHub repos
-                              </a>
-                              <a href="https://www.google.com/" target="_blank" class="f-white75 fw-400 d-block t-decor-none mt-5">
-                                  Community forum
-                              </a>
-                              <a href="https://www.google.com/" target="_blank" class="f-white75 fw-400 d-block t-decor-none mt-5">
-                                  Flashcard decks
-                              </a>
-                              <a href="https://www.google.com/" target="_blank" class="f-white75 fw-400 d-block t-decor-none mt-5">
-                                  New videos every week
-                              </a>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
-  </div>
+	<div class="body">
+        <div class="bg-pattern-1">
+            <img src=".\images\bg-pattern-top-desktop.svg" alt="BG Pattern 1">
+        </div>
+        <div class="bg-pattern-2">
+            <img src=".\images\bg-pattern-bottom-desktop.svg" alt="BG Pattern 2">
+        </div>
+		<div class="container">
+			<div class="container-card">
+				<div class="wrap-card">
+					<div class="wrap-content">
+						<div class="spacer">
+							<div class="wrap-title f-verydarkmagenta fw-700">
+								10,000+ of our users love our products.
+							</div>
+							<div class="wrap-description mt-20">
+								<span class="f-darkgrayishmagenta fw-500 d-block mt-20">
+									We only provide great products combined with excellent customer service.
+									See what our satisfied customers are saying about our services.
+								</span>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="wrap-card">
+					<div class="wrap-content">
+						<div class="spacer">
+							<div class="wrap-list-info">
+								<div class="info-item bg-lightgrayishmagenta">
+									<div class="info-item-img">
+										<img src="./images/icon-star.svg" alt="Icon Star" />
+										<img src="./images/icon-star.svg" alt="Icon Star" />
+										<img src="./images/icon-star.svg" alt="Icon Star" />
+										<img src="./images/icon-star.svg" alt="Icon Star" />
+										<img src="./images/icon-star.svg" alt="Icon Star" />
+									</div>
+									<span class="fw-700 d-block f-verydarkmagenta">
+										Rated 5 Stars in Reviews
+									</span>
+								</div>
+								<div class="info-item bg-lightgrayishmagenta">
+									<div class="info-item-img">
+										<img src="./images/icon-star.svg" alt="Icon Star" />
+										<img src="./images/icon-star.svg" alt="Icon Star" />
+										<img src="./images/icon-star.svg" alt="Icon Star" />
+										<img src="./images/icon-star.svg" alt="Icon Star" />
+										<img src="./images/icon-star.svg" alt="Icon Star" />
+									</div>
+									<span class="fw-700 d-block f-verydarkmagenta">
+										Rated 5 Stars in Report Guru
+									</span>
+								</div>
+								<div class="info-item bg-lightgrayishmagenta">
+									<div class="info-item-img">
+										<img src="./images/icon-star.svg" alt="Icon Star" />
+										<img src="./images/icon-star.svg" alt="Icon Star" />
+										<img src="./images/icon-star.svg" alt="Icon Star" />
+										<img src="./images/icon-star.svg" alt="Icon Star" />
+										<img src="./images/icon-star.svg" alt="Icon Star" />
+									</div>
+									<span class="fw-700 d-block f-verydarkmagenta">
+										Rated 5 Stars in BestTech
+									</span>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="wrap-card">
+					<div class="wrap-content">
+						<div class="spacer">
+							<div class="wrap-testimonial">
+								<div class="testimonial-item bg-verydarkmagenta">
+									<a href="https://www.google.com/" target="_blank" class="wrap-info-with-thumb">
+										<img src=".\images\image-colton.jpg" alt="">
+										<div class="text-info">
+											<span class="f-white fw-500">
+												Colton Smith
+											</span>
+											<span class="f-softpink fw-400">
+												Verified Buyer
+											</span>
+										</div>
+									</a>
+									<div class="wrap-description f-white fw-400 mt-20">
+										"We needed the same printed design as the one we had ordered a week prior.
+										Not only did they find the original order, but we also received it in time.
+										Excellent!"
+									</div>
+								</div>
+								<div class="testimonial-item bg-verydarkmagenta">
+									<a href="https://www.google.com/" target="_blank" class="wrap-info-with-thumb">
+										<img src=".\images\image-irene.jpg" alt="">
+										<div class="text-info">
+											<span class="f-white fw-500">
+												Irene Roberts
+											</span>
+											<span class="f-softpink fw-400">
+												Verified Buyer
+											</span>
+										</div>
+									</a>
+									<div class="wrap-description f-white fw-400 mt-20">
+										"Customer service is always excellent and very quick turn around. Completely
+										delighted with the simplicity of the purchase and the speed of delivery."
+									</div>
+								</div>
+								<div class="testimonial-item bg-verydarkmagenta">
+									<a href="https://www.google.com/" target="_blank" class="wrap-info-with-thumb">
+										<img src=".\images\image-anne.jpg" alt="">
+										<div class="text-info">
+											<span class="f-white fw-500">
+												Anne Wallace
+											</span>
+											<span class="f-softpink fw-400">
+												Verified Buyer
+											</span>
+										</div>
+									</a>
+									<div class="wrap-description f-white fw-400 mt-20">
+										"Put an order with this company and can only praise them for the very high
+										standard. Will definitely use them again and recommend them to everyone!"
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <style lang="scss" scoped>
-  @import'./style.scss';
+	@import'./style.scss';
 </style>
 
 <script setup>
-  useHead({
-      title: '010 Social proof section',
-  })
+	useHead({
+		title: '010 Social proof section',
+	})
 </script>
