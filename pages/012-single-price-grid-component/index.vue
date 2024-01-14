d62d4e7fccaaf8e51e256b7def3285781d79b6bb
saktiajadeh <saktiajadeh29@gmail.com>
2024-01-14 11:12:33 +0800

    finishing challenges 012-single-price-grid-component

--- pages/012-single-price-grid-component/index.vue
+++ pages/012-single-price-grid-component/index.vue
@@ -2,25 +2,75 @@
     <div class="body">
         <div class="container">
             <div class="container-card">
-                <div class="wrap-card brightorange">
+                <div class="wrap-card bg-white">
                     <div class="wrap-content">
                         <div class="spacer">
-                            <div class="wrap-img">
-                                <img src=".\images\icon-sedans.svg" alt="Icon Preview">
+                            <div class="wrap-title f-cyan fw-700">
+                                Join our community
                             </div>
-                            <div class="wrap-title">
-                                Sedans
-                            </div>
-                            <div class="wrap-description">
-                                Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in
-                                the city
-                                or on your next road trip.
+                            <div class="wrap-description mt-20">
+                                <span class="f-brightyellow fw-700">
+                                    30-day, hassle-free money back guarantee
+                                </span>
+                                <span class="f-grayishblue fw-400 d-block mt-20">
+                                    Gain access to our full library of tutorials along with expert code reviews. Perfect for any developers who are serious about honing their skills.
+                                </span>
                             </div>
                         </div>
-                        <div class="wrap-action">
-                            <a href="https://www.google.com/" target="_blank" class="wrap-button primary">
-                                <span>Learn More</span>
+                    </div>
+                </div>
+                <div class="wrap-card bg-cyan">
+                    <div class="wrap-content">
+                        <div class="spacer">
+                            <div class="wrap-title f-white fw-700">
+                                Monthly Subscription
+                            </div>
+                            <div class="wrap-price f-white mt-20">
+                                <span class="show-price fw-700">$29</span>
+                                <span class="disc-price ml-10">per month</span>
+                            </div>
+                            <div class="wrap-description mt-10">
+                                <span class="f-white fw-400 d-block mt-10">
+                                    Full access for less than $1 a day
+                                </span>
+                            </div>
+                        </div>
+                        <div class="wrap-action mt-20">
+                            <a href="https://www.google.com/" target="_blank" class="wrap-button bg-brightyellow f-white fw-700">
+                                <span>Sign Up</span>
                             </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="wrap-card bg-cyan70">
+                    <div class="wrap-content">
+                        <div class="spacer">
+                            <div class="wrap-title f-white fw-700">
+                                Why Us
+                            </div>
+                            <div class="wrap-description mt-20">
+                                <a href="https://www.google.com/" target="_blank" class="f-white75 fw-400 d-block t-decor-none">
+                                    Tutorials by industry experts
+                                </a>
+                                <a href="https://www.google.com/" target="_blank" class="f-white75 fw-400 d-block t-decor-none mt-5">
+                                    Peer & expert code reviews
+                                </a>
+                                <a href="https://www.google.com/" target="_blank" class="f-white75 fw-400 d-block t-decor-none mt-5">
+                                    Coding exercises
+                                </a>
+                                <a href="https://www.google.com/" target="_blank" class="f-white75 fw-400 d-block t-decor-none mt-5">
+                                    Access to our GitHub repos
+                                </a>
+                                <a href="https://www.google.com/" target="_blank" class="f-white75 fw-400 d-block t-decor-none mt-5">
+                                    Community forum
+                                </a>
+                                <a href="https://www.google.com/" target="_blank" class="f-white75 fw-400 d-block t-decor-none mt-5">
+                                    Flashcard decks
+                                </a>
+                                <a href="https://www.google.com/" target="_blank" class="f-white75 fw-400 d-block t-decor-none mt-5">
+                                    New videos every week
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
