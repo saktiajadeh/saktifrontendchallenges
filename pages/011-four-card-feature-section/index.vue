6f5852a02b3149fe03bed5eedd4709201071a131
saktiajadeh <saktiajadeh29@gmail.com>
2024-01-14 23:14:14 +0800

    finishing challenges 011-four-card-feature-section

--- pages/011-four-card-feature-section/index.vue
+++ pages/011-four-card-feature-section/index.vue
@@ -1,158 +1,62 @@
 <template>
-	<div class="body">
-        <div class="bg-pattern-1">
-            <img src=".\images\bg-pattern-top-desktop.svg" alt="BG Pattern 1">
+    <div class="body">
+        <div class="container">
+            <div class="container-card">
+                <div class="wrap-card">
+                    <div class="wrap-content">
+                        <div class="spacer">
+                            <div class="wrap-title f-verydarkmagenta">
+                                <span class="f-verydarkblue fw-200">
+                                    Reliable, efficient delivery
+                                </span>
+                                <br>
+                                <span class="f-verydarkblue fw-600">
+                                    Powered by Technology
+                                </span>
+                            </div>
+                            <div class="wrap-description mt-20">
+                                <span class="f-grayishblue fw-400 d-block mt-20">
+                                    Our Artificial Intelligence powered tools use millions of project data points
+                                    to ensure that your project is successful
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="wrap-card">
+                    <div class="wrap-content">
+                        <div class="spacer">
+                            <div class="wrap-list-column">
+                                <div class="list-item bg-white" v-for="row in data">
+                                    <div class="spacer">
+                                        <div class="wrap-title f-verydarkblue fw-600">
+                                            {{row.title}}
+                                        </div>
+                                        <div class="wrap-description f-verydarkblue f-grayishblue mt-20">
+                                            {{ row.description }}
+                                        </div>
+                                    </div>
+                                    <div class="wrap-img mt-30">
+                                        <img :src="row.image" alt=""/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="bg-pattern-2">
-            <img src=".\images\bg-pattern-bottom-desktop.svg" alt="BG Pattern 2">
-        </div>
-		<div class="container">
-			<div class="container-card">
-				<div class="wrap-card">
-					<div class="wrap-content">
-						<div class="spacer">
-							<div class="wrap-title f-verydarkmagenta fw-700">
-								10,000+ of our users love our products.
-							</div>
-							<div class="wrap-description mt-20">
-								<span class="f-darkgrayishmagenta fw-500 d-block mt-20">
-									Reliable, efficient delivery
-                  Powered by Technology
-
-                  Our Artificial Intelligence powered tools use millions of project data points 
-                  to ensure that your project is successful
-
-                  Supervisor
-                  Monitors activity to identify project roadblocks
-
-                  Team Builder
-                  Scans our talent network to create the optimal team for your project
-
-                  Karma
-                  Regularly evaluates our talent to ensure quality
-
-                  Calculator
-                  Uses data from past projects to provide better delivery estimates
-								</span>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="wrap-card">
-					<div class="wrap-content">
-						<div class="spacer">
-							<div class="wrap-list-info">
-								<div class="info-item bg-lightgrayishmagenta">
-									<div class="info-item-img">
-										<img src="./images/icon-star.svg" alt="Icon Star" />
-										<img src="./images/icon-star.svg" alt="Icon Star" />
-										<img src="./images/icon-star.svg" alt="Icon Star" />
-										<img src="./images/icon-star.svg" alt="Icon Star" />
-										<img src="./images/icon-star.svg" alt="Icon Star" />
-									</div>
-									<span class="fw-700 d-block f-verydarkmagenta">
-										Rated 5 Stars in Reviews
-									</span>
-								</div>
-								<div class="info-item bg-lightgrayishmagenta">
-									<div class="info-item-img">
-										<img src="./images/icon-star.svg" alt="Icon Star" />
-										<img src="./images/icon-star.svg" alt="Icon Star" />
-										<img src="./images/icon-star.svg" alt="Icon Star" />
-										<img src="./images/icon-star.svg" alt="Icon Star" />
-										<img src="./images/icon-star.svg" alt="Icon Star" />
-									</div>
-									<span class="fw-700 d-block f-verydarkmagenta">
-										Rated 5 Stars in Report Guru
-									</span>
-								</div>
-								<div class="info-item bg-lightgrayishmagenta">
-									<div class="info-item-img">
-										<img src="./images/icon-star.svg" alt="Icon Star" />
-										<img src="./images/icon-star.svg" alt="Icon Star" />
-										<img src="./images/icon-star.svg" alt="Icon Star" />
-										<img src="./images/icon-star.svg" alt="Icon Star" />
-										<img src="./images/icon-star.svg" alt="Icon Star" />
-									</div>
-									<span class="fw-700 d-block f-verydarkmagenta">
-										Rated 5 Stars in BestTech
-									</span>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="wrap-card">
-					<div class="wrap-content">
-						<div class="spacer">
-							<div class="wrap-testimonial">
-								<div class="testimonial-item bg-verydarkmagenta">
-									<a href="https://www.google.com/" target="_blank" class="wrap-info-with-thumb">
-										<img src=".\images\image-colton.jpg" alt="">
-										<div class="text-info">
-											<span class="f-white fw-500">
-												Colton Smith
-											</span>
-											<span class="f-softpink fw-400">
-												Verified Buyer
-											</span>
-										</div>
-									</a>
-									<div class="wrap-description f-white fw-400 mt-20">
-										"We needed the same printed design as the one we had ordered a week prior.
-										Not only did they find the original order, but we also received it in time.
-										Excellent!"
-									</div>
-								</div>
-								<div class="testimonial-item bg-verydarkmagenta">
-									<a href="https://www.google.com/" target="_blank" class="wrap-info-with-thumb">
-										<img src=".\images\image-irene.jpg" alt="">
-										<div class="text-info">
-											<span class="f-white fw-500">
-												Irene Roberts
-											</span>
-											<span class="f-softpink fw-400">
-												Verified Buyer
-											</span>
-										</div>
-									</a>
-									<div class="wrap-description f-white fw-400 mt-20">
-										"Customer service is always excellent and very quick turn around. Completely
-										delighted with the simplicity of the purchase and the speed of delivery."
-									</div>
-								</div>
-								<div class="testimonial-item bg-verydarkmagenta">
-									<a href="https://www.google.com/" target="_blank" class="wrap-info-with-thumb">
-										<img src=".\images\image-anne.jpg" alt="">
-										<div class="text-info">
-											<span class="f-white fw-500">
-												Anne Wallace
-											</span>
-											<span class="f-softpink fw-400">
-												Verified Buyer
-											</span>
-										</div>
-									</a>
-									<div class="wrap-description f-white fw-400 mt-20">
-										"Put an order with this company and can only praise them for the very high
-										standard. Will definitely use them again and recommend them to everyone!"
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
-	@import'./style.scss';
+    @import'./style.scss';
 </style>
 
 <script setup>
-	useHead({
-		title: '012 Single price grid component',
-	})
+    useHead({
+        title: '011 Four card feature section',
+    });
+
+    import { data } from './scripts.js';
 </script>
---
+++ pages/011-four-card-feature-section/scripts.js
@@ -0,0 +1,21 @@
+export const data = [{
+        title: 'Supervisor',
+        description: 'Monitors activity to identify project roadblocks',
+        image: '_nuxt/pages/011-four-card-feature-section/images/icon-supervisor.svg',
+    },
+    {
+        title: 'Team Builder',
+        description: 'Scans our talent network to create the optimal team for your project',
+        image: '_nuxt/pages/011-four-card-feature-section/images/icon-team-builder.svg'
+    },
+    {
+        title: 'Karma',
+        description: 'Regularly evaluates our talent to ensure quality',
+        image: '_nuxt/pages/011-four-card-feature-section/images/icon-karma.svg'
+    },
+    {
+        title: 'Calculator',
+        description: 'Uses data from past projects to provide better delivery estimates',
+        image: '_nuxt/pages/011-four-card-feature-section/images/icon-calculator.svg'
+    }
+];
