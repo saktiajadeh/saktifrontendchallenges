d26bd96096ef77639e3dd346018f3ad450f28420
saktiajadeh <saktiajadeh29@gmail.com>
2024-01-17 08:37:03 +0800

    finishing challenges 013-huddle-landing-page-with-single-introductory-section

--- pages/013-huddle-landing-page-with-single-introductory-section/index.vue
+++ pages/013-huddle-landing-page-with-single-introductory-section/index.vue
@@ -1,48 +1,54 @@
 <template>
     <main class="wrapper">
-        <figure class="absolute-bg">
+        <figure class="absolute-bg animate fadeInScaleFromTop">
             <img src="./images/bg-mobile.svg">
         </figure>
-
-        <header class="header">
-            <a href="https://www.google.com/" target="_blank" class="header-image">
-                <img class="" src="./images/logo.svg" alt="Logo Huddle" />
-            </a>
-        </header>
-
-        <section class="section-content">
-            <figure class="figure-mockup">
-                <img class="" src="./images/illustration-mockups.svg" alt="Illustration Mockups" />
-            </figure>
-
-            <article class="article-content">
-                <h2>Build The Community Your Fans Will Love</h2>
-                <p>
-                    Huddle re-imagines the way we build communities. You have a voice, but so does your audience.
-                    Create connections with your users as you engage in genuine discussion.
-                </p>
-            </article>
-
-            <nav class="wrapper-action">
-                <a href="https://www.google.com/" target="_blank" class="btn-main">
-                    Register
+        <figure class="absolute-bg-desktop animate fadeInScaleFromLeft">
+            <img src="./images/bg-desktop.svg">
+        </figure>
+        <section class="contain">
+            <header class="header">
+                <a href="https://www.google.com/" target="_blank" class="header-image">
+                    <img class="" src="./images/logo.svg" alt="Logo Huddle" />
                 </a>
-            </nav>
+            </header>
+
+            <section class="section-content">
+                <article class="layout-grid">
+                    <figure class="figure-mockup">
+                        <img class="" src="./images/illustration-mockups.svg" alt="Illustration Mockups" />
+                    </figure>
+
+                    <article class="article-content">
+                        <h2>Build The Community Your Fans Will Love</h2>
+                        <p>
+                            Huddle re-imagines the way we build communities. You have a voice, but so does your
+                            audience.
+                            Create connections with your users as you engage in genuine discussion.
+                        </p>
+                        <nav class="wrapper-action">
+                            <a href="https://www.google.com/" target="_blank" class="btn-main">
+                                Register
+                            </a>
+                        </nav>
+                    </article>
+                </article>
+            </section>
+
+            <footer class="footer">
+                <nav class="social-links">
+                    <a href="https://www.google.com/" target="_blank" class="btn-circle-border">
+                        <i class="ion-social-facebook"></i>
+                    </a>
+                    <a href="https://www.google.com/" target="_blank" class="btn-circle-border">
+                        <i class="ion-social-twitter"></i>
+                    </a>
+                    <a href="https://www.google.com/" target="_blank" class="btn-circle-border">
+                        <i class="ion-social-instagram"></i>
+                    </a>
+                </nav>
+            </footer>
         </section>
-
-        <footer class="footer">
-            <nav class="social-links">
-                <a href="https://www.google.com/" target="_blank" class="btn-circle">
-                    <i class="ion-social-facebook"></i>
-                </a>
-                <a href="https://www.google.com/" target="_blank" class="btn-circle">
-                    <i class="ion-social-twitter"></i>
-                </a>
-                <a href="https://www.google.com/" target="_blank" class="btn-circle">
-                    <i class="ion-social-instagram"></i>
-                </a>
-            </nav>
-        </footer>
     </main>
 </template>
 
