fb644acf7cc14d1ad63fae18ea4b800dadab97d1
saktiajadeh <saktiajadeh29@gmail.com>
2024-01-17 09:40:13 +0800

    update styling index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,32 @@
 <template>
-    <div class="body">
-        <div class="container">
-            <ul class="styled-list">
-                <li><NuxtLink to="/001-blog-preview-card">001 Blog Preview Card</NuxtLink></li>
-                <li><NuxtLink to="/002-results-summary-component">002 Results Summary Component</NuxtLink></li>
-                <li><NuxtLink to="/003-product-preview-card-component">003 Product preview card component</NuxtLink></li>
-                <li><NuxtLink to="/004-qr-code-component">004 QR code component</NuxtLink></li>
-                <li><NuxtLink to="/005-nft-preview-card-component">005 NFT preview card component</NuxtLink></li>
-                <li><NuxtLink to="/006-order-summary-component">006 Order summary component</NuxtLink></li>
-                <li><NuxtLink to="/007-stats-preview-card-component">007 Stats preview card component</NuxtLink></li>
-                <li><NuxtLink to="/008-3-column-preview-card-component">008 3-column preview card component</NuxtLink></li>
-                <li><NuxtLink to="/009-profile-card-component">009 Profile card component</NuxtLink></li>
-                <li><NuxtLink to="/010-social-proof-section">010 Social proof section</NuxtLink></li>
-                <li><NuxtLink to="/011-four-card-feature-section">011 Four card feature section</NuxtLink></li>
-                <li><NuxtLink to="/012-single-price-grid-component">012 Single price grid component</NuxtLink></li>
-                <li><NuxtLink to="/013-huddle-landing-page-with-single-introductory-section">013 Huddle landing page with a single introductory section</NuxtLink></li>
-            </ul>
-            <div class="attribution">
-                Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
-                Coded by <a href="https://github.com/saktiajadeh">Sakti</a>.
-            </div>
-        </div>
-    </div>
+    <main class="wrapper">
+        <section class="contain">
+            <article class="article-list">
+                <ul class="styled-list">
+                    <li><NuxtLink to="/001-blog-preview-card">001 Blog Preview Card</NuxtLink></li>
+                    <li><NuxtLink to="/002-results-summary-component">002 Results Summary Component</NuxtLink></li>
+                    <li><NuxtLink to="/003-product-preview-card-component">003 Product preview card component</NuxtLink></li>
+                    <li><NuxtLink to="/004-qr-code-component">004 QR code component</NuxtLink></li>
+                    <li><NuxtLink to="/005-nft-preview-card-component">005 NFT preview card component</NuxtLink></li>
+                    <li><NuxtLink to="/006-order-summary-component">006 Order summary component</NuxtLink></li>
+                    <li><NuxtLink to="/007-stats-preview-card-component">007 Stats preview card component</NuxtLink></li>
+                    <li><NuxtLink to="/008-3-column-preview-card-component">008 3-column preview card component</NuxtLink></li>
+                    <li><NuxtLink to="/009-profile-card-component">009 Profile card component</NuxtLink></li>
+                    <li><NuxtLink to="/010-social-proof-section">010 Social proof section</NuxtLink></li>
+                    <li><NuxtLink to="/011-four-card-feature-section">011 Four card feature section</NuxtLink></li>
+                    <li><NuxtLink to="/012-single-price-grid-component">012 Single price grid component</NuxtLink></li>
+                    <li><NuxtLink to="/013-huddle-landing-page-with-single-introductory-section">013 Huddle landing page with a single introductory section</NuxtLink></li>
+                </ul> 
+            </article>
+
+            <footer class="attribution">
+                <p>
+                    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"><u>Frontend Mentor</u></a>.
+                    Coded by <a href="https://github.com/saktiajadeh"><u>Sakti</u></a>.
+                </p>
+            </footer>
+        </section>
+    </main>
 </template>
 
 <style lang="scss" scoped>
