4b24a1cc60596cf2d93eafa63e25f784e10c22e9
saktiajadeh <saktiajadeh29@gmail.com>
2024-01-20 06:42:27 +0800

    add started files 015-recipe-page

---
+++ pages/015-recipe-page/index.vue
@@ -0,0 +1,136 @@
+<template>
+    <main class="wrapper">
+        Simple Omelette Recipe
+
+        An easy and quick dish, perfect for any meal. This classic omelette combines beaten eggs cooked
+        to perfection, optionally filled with your choice of cheese, vegetables, or meats.
+
+        Preparation time
+
+        Total: Approximately 10 minutes
+        Preparation: 5 minutes
+        Cooking: 5 minutes
+
+        Ingredients
+
+        2-3 large eggs
+        Salt, to taste
+        Pepper, to taste
+        1 tablespoon of butter or oil
+        Optional fillings: cheese, diced vegetables, cooked meats, herbs
+
+        Instructions
+
+        Beat the eggs: In a bowl, beat the eggs with a pinch of salt and pepper until they are well mixed.
+        You can add a tablespoon of water or milk for a fluffier texture.
+
+        Heat the pan: Place a non-stick frying pan over medium heat and add butter or oil.
+
+        Cook the omelette: Once the butter is melted and bubbling, pour in the eggs. Tilt the pan to ensure
+        the eggs evenly coat the surface.
+
+        Add fillings (optional): When the eggs begin to set at the edges but are still slightly runny in the
+        middle, sprinkle your chosen fillings over one half of the omelette.
+
+        Fold and serve: As the omelette continues to cook, carefully lift one edge and fold it over the
+        fillings. Let it cook for another minute, then slide it onto a plate.
+
+        Enjoy: Serve hot, with additional salt and pepper if needed.
+
+        Nutrition
+
+        The table below shows nutritional values per serving without the additional fillings.
+
+        Calories
+        277kcal
+
+        Carbs
+        0g
+
+        Protein
+        20g
+
+        Fat
+        22g
+        <!-- <figure class="absolute-bg">
+            <img class="bg-mobile" src="./assets/images/background-pattern-mobile.svg" alt="Background Pattern Mobile">
+            <img class="bg-desktop" src="./assets/images/background-pattern-desktop.svg"
+                alt="Background Pattern Desktop">
+        </figure>
+        <section class="contain">
+            <section class="section-content">
+                <article class="wrap-title">
+                    <figure class="wrap-image">
+                        <img src="./assets/images/icon-star.svg" alt="Icon Star">
+                    </figure>
+                    <h2 class="title">
+                        FAQs
+                    </h2>
+                </article>
+
+                <article class="wrap-list">
+                    <ul class="accordion-list">
+                        <li class="list-item" v-for="(row, index) in accordionList" :key="index"
+                            :class="{'active': row.isActive}" @keydown.enter="toggleAccordion(index)">
+                            <article class="title-list-item">
+                                <h6 class="title" @click="toggleAccordion(index)" tabindex="0">
+                                    {{ row.title }}
+                                </h6>
+                                <button class="btn-circle" @click="toggleAccordion(index)">
+                                    <i class="ion-minus-round"></i>
+                                    <i class="ion-plus-round"></i>
+                                </button>
+                            </article>
+                            <article class="content-list-item animate fadeInScaleFromTop">
+                                <p class="description">
+                                    {{ row.description }}
+                                </p>
+                            </article>
+                        </li>
+                    </ul>
+                </article>
+            </section>
+        </section> -->
+    </main>
+</template>
+
+<style lang="scss" scoped>
+    @import'./style.scss';
+</style>
+
+<script setup>
+    useHead({
+        title: '015 Recipe page',
+    });
+
+    const toggleAccordion = (index) => {
+        accordionList.value[index].isActive = !accordionList.value[index].isActive;
+    };
+
+    const accordionList = ref([{
+            title: 'What is Frontend Mentor, and how will it help me?',
+            description: `Frontend Mentor offers realistic coding challenges to help developers improve their 
+          frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for 
+          all levels and ideal for portfolio building.`,
+            isActive: true,
+        },
+        {
+            title: 'Is Frontend Mentor free?',
+            description: `Yes, Frontend Mentor offers both free and premium coding challenges, with the free 
+          option providing access to a range of projects suitable for all skill levels.`,
+            isActive: false,
+        },
+        {
+            title: 'Can I use Frontend Mentor projects in my portfolio?',
+            description: `Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent
+          way to showcase your skills to potential employers!`,
+            isActive: false,
+        },
+        {
+            title: `How can I get help if I'm stuck on a Frontend Mentor challenge?`,
+            description: `The best place to get help is inside Frontend Mentor's Discord community. There's a help 
+          channel where you can ask questions and seek support from other community members.`,
+            isActive: false,
+        },
+    ]);
+</script>
