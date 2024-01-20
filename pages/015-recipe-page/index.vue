6f2ec83f6c32a525b61273a16cbd200bf63c3588
saktiajadeh <saktiajadeh29@gmail.com>
2024-01-20 08:55:51 +0800

    finishing challenges 015-recipe-page

--- pages/015-recipe-page/index.vue
+++ pages/015-recipe-page/index.vue
@@ -1,57 +1,116 @@
 <template>
     <main class="wrapper">
-        Simple Omelette Recipe
-
-        An easy and quick dish, perfect for any meal. This classic omelette combines beaten eggs cooked
-        to perfection, optionally filled with your choice of cheese, vegetables, or meats.
-
-        Preparation time
-
-        Total: Approximately 10 minutes
-        Preparation: 5 minutes
-        Cooking: 5 minutes
-
-        Ingredients
-
-        2-3 large eggs
-        Salt, to taste
-        Pepper, to taste
-        1 tablespoon of butter or oil
-        Optional fillings: cheese, diced vegetables, cooked meats, herbs
-
-        Instructions
-
-        Beat the eggs: In a bowl, beat the eggs with a pinch of salt and pepper until they are well mixed.
-        You can add a tablespoon of water or milk for a fluffier texture.
-
-        Heat the pan: Place a non-stick frying pan over medium heat and add butter or oil.
-
-        Cook the omelette: Once the butter is melted and bubbling, pour in the eggs. Tilt the pan to ensure
-        the eggs evenly coat the surface.
-
-        Add fillings (optional): When the eggs begin to set at the edges but are still slightly runny in the
-        middle, sprinkle your chosen fillings over one half of the omelette.
-
-        Fold and serve: As the omelette continues to cook, carefully lift one edge and fold it over the
-        fillings. Let it cook for another minute, then slide it onto a plate.
-
-        Enjoy: Serve hot, with additional salt and pepper if needed.
-
-        Nutrition
-
-        The table below shows nutritional values per serving without the additional fillings.
-
-        Calories
-        277kcal
-
-        Carbs
-        0g
-
-        Protein
-        20g
-
-        Fat
-        22g
+        <section class="contain">
+            <section class="section-content">
+                <figure class="wrap-img">
+                    <img src="./assets/images/image-omelette.jpeg" alt="Image omelette">
+                </figure>
+                <article class="wrap-text">
+                    <h1 class="main-title">
+                        Simple Omelette Recipe
+                    </h1>
+                    <p class="main-description">
+                        An easy and quick dish, perfect for any meal. This classic omelette combines beaten eggs cooked
+                        to perfection, optionally filled with your choice of cheese, vegetables, or meats.
+                    </p>
+                    <article class="wrap-highlight-text">
+                        <h3 class="highlight-title">
+                            Preparation time
+                        </h3>
+                        <ul class="highlight-list">
+                            <li>
+                                <span>Total:</span> Approximately 10 minutes
+                            </li>
+                            <li>
+                                <span>Preparation:</span> 5 minutes
+                            </li>
+                            <li>
+                                <span>Cooking:</span> 5 minutes
+                            </li>
+                        </ul>
+                    </article>
+                    <article class="wrap-step-text">
+                        <h2 class="step-title">
+                            Ingredients
+                        </h2>
+                        <ul class="step-list disc">
+                            <li>
+                                2-3 large eggs
+                            </li>
+                            <li>
+                                Salt, to taste
+                            </li>
+                            <li>
+                                Pepper, to taste
+                            </li>
+                            <li>
+                                1 tablespoon of butter or oil
+                            </li>
+                            <li>
+                                Optional fillings: cheese, diced vegetables, cooked meats, herbs
+                            </li>
+                        </ul>
+                    </article>
+                    <article class="wrap-step-text">
+                        <h2 class="step-title">
+                            Instructions
+                        </h2>
+                        <ol class="step-list">
+                            <li>
+                                <span>Beat the eggs:</span> In a bowl, beat the eggs with a pinch of salt and pepper until they are well mixed.
+                                You can add a tablespoon of water or milk for a fluffier texture.
+                            </li>
+                            <li>
+                                <span>Heat the pan:</span> Place a non-stick frying pan over medium heat and add butter or oil.
+                            </li>
+                            <li>
+                                <span>Cook the omelette:</span> Once the butter is melted and bubbling, pour in the eggs. Tilt the pan to ensure
+                                the eggs evenly coat the surface.
+                            </li>
+                            <li>
+                                <span>Add fillings (optional):</span> When the eggs begin to set at the edges but are still slightly runny in the
+                                middle, sprinkle your chosen fillings over one half of the omelette.
+                            </li>
+                            <li>
+                                <span>Fold and serve:</span> As the omelette continues to cook, carefully lift one edge and fold it over the
+                                fillings. Let it cook for another minute, then slide it onto a plate.
+                            </li>
+                            <li>
+                                <span>Enjoy:</span> Serve hot, with additional salt and pepper if needed.
+                            </li>
+                        </ol>
+                    </article>
+                    <article class="wrap-step-text">
+                        <h2 class="step-title">
+                            Nutrition
+                        </h2>
+                        <p class="step-description">
+                            The table below shows nutritional values per serving without the additional fillings.
+                        </p>
+                        <article class="wrap-table">
+                            <table>
+                                <tr>
+                                    <td>Calories</td>
+                                    <td class="text-highlight">277kcal</td>
+                                </tr>
+                                <tr>
+                                    <td>Cabs</td>
+                                    <td class="text-highlight">0g</td>
+                                </tr>
+                                <tr>
+                                    <td>Protein</td>
+                                    <td class="text-highlight">20g</td>
+                                </tr>
+                                <tr>
+                                    <td>Fat</td>
+                                    <td class="text-highlight">22g</td>
+                                </tr>
+                            </table>
+                        </article>
+                    </article>
+                </article>
+            </section>
+        </section>
         <!-- <figure class="absolute-bg">
             <img class="bg-mobile" src="./assets/images/background-pattern-mobile.svg" alt="Background Pattern Mobile">
             <img class="bg-desktop" src="./assets/images/background-pattern-desktop.svg"
@@ -102,10 +161,6 @@
     useHead({
         title: '015 Recipe page',
     });
-
-    const toggleAccordion = (index) => {
-        accordionList.value[index].isActive = !accordionList.value[index].isActive;
-    };
 
     const accordionList = ref([{
             title: 'What is Frontend Mentor, and how will it help me?',
