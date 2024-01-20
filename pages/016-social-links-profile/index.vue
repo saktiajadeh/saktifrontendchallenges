8bc892538c47ad47da40dfd41efc10bf96c5d45c
saktiajadeh <saktiajadeh29@gmail.com>
2024-01-20 12:16:01 +0800

    finishing challenges 016-social-links-profile

--- pages/016-social-links-profile/index.vue
+++ pages/016-social-links-profile/index.vue
@@ -1,66 +1,60 @@
 <template>
-  <main class="wrapper">
-      <section class="contain">
-          <section class="section-content">
-              <!-- <figure class="wrap-img">
-                  <img src="./assets/images/image-omelette.jpeg" alt="Image omelette">
-              </figure> -->
-              <article class="wrap-text">
-                  <!-- <h1 class="main-title">
-                      Simple Omelette Recipe
-                  </h1>
-                  <p class="main-description">
-                      An easy and quick dish, perfect for any meal. This classic omelette combines beaten eggs cooked
-                      to perfection, optionally filled with your choice of cheese, vegetables, or meats.
-                  </p> -->
-                  Jessica Randall
-                  London, United Kingdom
-                  "Front-end developer and avid reader."
-
-                  GitHub
-                  Frontend Mentor
-                  LinkedIn
-                  Twitter
-                  Instagram
-              </article>
-          </section>
-      </section>
-  </main>
+    <main class="wrapper">
+        <section class="contain">
+            <section class="section-content">
+                <figure class="wrap-img">
+                    <img src="./assets/images/avatar-jessica.jpeg" alt="Image omelette">
+                </figure>
+                <article class="wrap-text">
+                    <h1 class="main-title">
+                        Jessica Randall
+                    </h1>
+                    <h2 class="main-highlight">
+                        London, United Kingdom
+                    </h2>
+                    <p class="main-description">
+                        "Front-end developer and avid reader."
+                    </p>
+                    <nav class="wrap-button">
+                        <a :href="row.url" target="_blank" class="btn-main" v-for="(row, index) in buttonList"
+                            :key="index">
+                            {{ row.title }}
+                        </a>
+                    </nav>
+                </article>
+            </section>
+        </section>
+    </main>
 </template>
 
 <style lang="scss" scoped>
-  @import'./style.scss';
+    @import'./style.scss';
 </style>
 
 <script setup>
-  useHead({
-      title: '016 Social links profile',
-  });
+    useHead({
+        title: '016 Social links profile',
+    });
 
-  const accordionList = ref([{
-          title: 'What is Frontend Mentor, and how will it help me?',
-          description: `Frontend Mentor offers realistic coding challenges to help developers improve their 
-        frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for 
-        all levels and ideal for portfolio building.`,
-          isActive: true,
-      },
-      {
-          title: 'Is Frontend Mentor free?',
-          description: `Yes, Frontend Mentor offers both free and premium coding challenges, with the free 
-        option providing access to a range of projects suitable for all skill levels.`,
-          isActive: false,
-      },
-      {
-          title: 'Can I use Frontend Mentor projects in my portfolio?',
-          description: `Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent
-        way to showcase your skills to potential employers!`,
-          isActive: false,
-      },
-      {
-          title: `How can I get help if I'm stuck on a Frontend Mentor challenge?`,
-          description: `The best place to get help is inside Frontend Mentor's Discord community. There's a help 
-        channel where you can ask questions and seek support from other community members.`,
-          isActive: false,
-      },
-  ]);
+    const buttonList = ref([{
+            title: 'GitHub',
+            url: '#',
+        },
+        {
+            title: 'Frontend Mentor',
+            url: '#',
+        },
+        {
+            title: 'LinkedIn',
+            url: '#',
+        },
+        {
+            title: 'Twitter',
+            url: '#',
+        },
+        {
+            title: 'Instagram',
+            url: '#',
+        },
+    ]);
 </script>
